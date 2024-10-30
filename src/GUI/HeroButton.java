088c66bcb7231c45953809d1041766ac09ac035e
florentin1210 <m.florentin419@gmail.com>
2024-10-31 01:52:14 +0200

    Worked on Board.java

--- src/GUI/HeroButton.java
+++ src/GUI/HeroButton.java
@@ -1,4 +1,45 @@
 package GUI;
 
-public class heroButton {
+import javax.swing.*;
+import java.awt.*;
+
+public class HeroButton extends JButton {
+    private Image backgroundImage;
+    private int atk;
+    private int hp;
+
+    public HeroButton(Image backgroundImage, int atk, int hp, int cost) {
+        this.backgroundImage = backgroundImage;
+        this.atk = atk;
+        this.hp = hp;
+        setContentAreaFilled(false);
+        setFocusPainted(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
+
+        g.setFont(new Font("Arial", Font.BOLD, 14));
+        g.setColor(Color.WHITE);
+
+        g.drawString(String.valueOf(atk), 10, getHeight() - 10);
+
+        g.drawString(String.valueOf(hp), getWidth() - 20, getHeight() - 10);
+
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+        repaint();
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+        repaint();
+    }
 }
